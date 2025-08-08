708a393de09c1977553fe8a6eaa92ecb4cd2b20c
IGuTSI <fatihkarakullukcu35@gmail.com>
2025-08-08 22:39:00 +0300

    Lesson 01

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+
+
+app.get('/', (req,res)=>{
+    res.status(200).send('INDEX SAYFASI');
+});
+
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
+
