db6176b4c593512cbc16ed4d1e512ce9ee5de567
IGuTSI <fatihkarakullukcu35@gmail.com>
2025-08-10 17:54:04 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -4,8 +4,32 @@ const app = express();
 
 
 
+
+
+
+//Template Engine
+app.set("view engine", "ejs"); //Template Engine'mizin ejs olduğunu söyledik
+
+
+//MiddleWares
+app.use(express.static("public"));//static dosyamızı public klasörünün içeriisnde olacak diyoruz. ve public klasörü oluşturuyoruz.
+
+
+
+
+//Routes (yönlendirmeler)
 app.get('/', (req,res)=>{
-    res.status(200).send('INDEX SAYFASI');
+    // res.status(200).send('INDEX SAYFASI');
+    res.status(200).render('index', {
+      page_name: "index"
+    }); // index.ejs sayfasını çalıştırdık
+});
+
+app.get('/about', (req,res)=>{
+    // res.status(200).send('INDEX SAYFASI');
+    res.status(200).render('about', {
+      page_name: "about"
+    }); // index.ejs sayfasını çalıştırdık
 });
 
 
